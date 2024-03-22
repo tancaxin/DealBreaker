cbae088dbe866e8622158fc992a5d732a647fc97
tancaxin <caxintan@gmail.com>
2024-03-22 15:13:00 +0800

    trade main activity done

--- app/src/main/java/com/example/investiq/TradeMainActivity.java
+++ app/src/main/java/com/example/investiq/TradeMainActivity.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
@@ -17,6 +18,9 @@ public class TradeMainActivity extends AppCompatActivity {
 
     LinearLayout screen;
     ImageView guidance;
+
+    Button buy;
+    Button sell;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,21 +28,88 @@ public class TradeMainActivity extends AppCompatActivity {
 
         screen = findViewById(R.id.screen);
         guidance = findViewById(R.id.btnGuidanceOne);
+        buy = findViewById(R.id.btnBuy);
+        sell = findViewById(R.id.btnSell);
 
         guidance.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CreatePopUpWindow();
+                CreatePopUpWindowGuideline();
+            }
+        });
+
+        buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreatePopUpWindowBuy();
+            }
+        });
+
+        sell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreatePopUpWindowSell();
             }
         });
     }
 
-    private void CreatePopUpWindow() {
+    private void CreatePopUpWindowSell() {
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popUpView = inflater.inflate(R.layout.sell_btn_popup, null);
+
+        int width = ViewGroup.LayoutParams.MATCH_PARENT;
+        int height = ViewGroup.LayoutParams.MATCH_PARENT;
+        boolean focusable = true;
+        PopupWindow popupWindow = new PopupWindow(popUpView, width, height, focusable);
+        screen.post(new Runnable() {
+            @Override
+            public void run() {
+                popupWindow.showAtLocation(screen, Gravity.CENTER,0,0);
+            }
+        });
+
+        popUpView.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
+    }
+
+    private void CreatePopUpWindowBuy() {
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popUpView = inflater.inflate(R.layout.buy_btn_popup, null);
+
+        int width = ViewGroup.LayoutParams.MATCH_PARENT;
+        int height = ViewGroup.LayoutParams.MATCH_PARENT;
+        boolean focusable = true;
+        PopupWindow popupWindow = new PopupWindow(popUpView, width, height, focusable);
+        screen.post(new Runnable() {
+            @Override
+            public void run() {
+                popupWindow.showAtLocation(screen, Gravity.CENTER,0,0);
+            }
+        });
+
+        popUpView.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
+
+    }
+
+    private void CreatePopUpWindowGuideline() {
         LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
         View popUpView = inflater.inflate(R.layout.trading_guidance_popup, null);
 
-        int width = ViewGroup.LayoutParams.WRAP_CONTENT;
-        int height = ViewGroup.LayoutParams.WRAP_CONTENT;
+        int width = ViewGroup.LayoutParams.MATCH_PARENT;
+        int height = ViewGroup.LayoutParams.MATCH_PARENT;
         boolean focusable = true;
         PopupWindow popupWindow = new PopupWindow(popUpView, width, height, focusable);
         screen.post(new Runnable() {
