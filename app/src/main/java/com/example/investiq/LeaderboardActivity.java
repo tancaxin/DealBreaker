f97dd6cd7f98aba9c81bd7de82912862cfab25ad
tancaxin <caxintan@gmail.com>
2024-03-23 18:21:09 +0800

    leaderboard done

---
+++ app/src/main/java/com/example/investiq/LeaderboardActivity.java
@@ -0,0 +1,60 @@
+package com.example.investiq;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LeaderboardActivity extends AppCompatActivity {
+    LinearLayout screen;
+    ImageView btnTradingRule;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaderboard);
+
+        screen = findViewById(R.id.screen);
+        btnTradingRule = findViewById(R.id.btnTradingRule);
+
+        btnTradingRule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreatePopUpWindowRules();
+            }
+        });
+
+    }
+
+    private void CreatePopUpWindowRules() {
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popUpView = inflater.inflate(R.layout.leaderboard_rules_popup, null);
+
+        int width = ViewGroup.LayoutParams.MATCH_PARENT;
+        int height = ViewGroup.LayoutParams.MATCH_PARENT;
+        boolean focusable = true;
+        PopupWindow popupWindow = new PopupWindow(popUpView, width, height, focusable);
+        screen.post(new Runnable() {
+            @Override
+            public void run() {
+                popupWindow.showAtLocation(screen, Gravity.CENTER,0,0);
+            }
+        });
+
+        popUpView.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
+    }
+}
