a5c1f02c704219d446413d31b629a9641f843456
tancaxin <caxintan@gmail.com>
2024-03-23 20:19:20 +0800

    put navigation and make adjustments

--- app/src/main/java/com/example/investiq/home.java
+++ app/src/main/java/com/example/investiq/home.java
@@ -1,6 +1,9 @@
 package com.example.investiq;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class home extends AppCompatActivity {
 
+    TextView txtLeaderboard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        txtLeaderboard = findViewById(R.id.txtLeaderboard);
+
+        txtLeaderboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(home.this, LeaderboardActivity.class);
+                startActivity(intent);
+            }
         });
+
     }
 }
